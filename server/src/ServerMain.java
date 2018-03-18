fd87cb3351dc2156867a33c90b7d16525a3a863d
1PALADIN1 <sonikx94@yandex.ru>
2018-03-18 14:18:55 +0300

    Добавил прослушку порта

--- server/src/ServerMain.java
+++ server/src/ServerMain.java
@@ -1,9 +1,42 @@
+import java.io.DataInputStream;
+import java.io.DataOutput;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 public class ServerMain {
+    private static Socket socket;
+    private static ServerSocket serverSocket;
+    private static DataInputStream dataInputStream;
+    private static DataOutputStream dataOutputStream;
+    private final static int SERVER_PORT = 5654;
+
     public static void main(String[] args) {
-        System.out.println("Start server...");
+        System.out.println("Запуск сервера...");
 
+        //открываем подключение к хранилищу
+        try {
+            serverSocket = new ServerSocket(SERVER_PORT);
+            System.out.println("Сервер запущен");
 
-        System.out.println("Server started");
+            while (true) {
+                socket = serverSocket.accept();
+                dataInputStream = new DataInputStream(socket.getInputStream());
+                dataOutputStream = new DataOutputStream(socket.getOutputStream());
+                System.out.println(dataInputStream.readUTF());
+                dataOutputStream.writeUTF("Бла-бла!");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (socket != null && !socket.isClosed()) socket.close();
+                if (serverSocket != null && !serverSocket.isClosed()) serverSocket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
     }
 }
