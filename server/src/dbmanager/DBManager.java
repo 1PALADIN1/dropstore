f34a57817c73d869bf421af4be3388913cf8564c
1PALADIN1 <sonikx94@yandex.ru>
2018-03-27 11:23:45 +0300

    Написал метод для проверки существования данных в БД (checkExistence)

--- server/src/dbmanager/DBManager.java
+++ server/src/dbmanager/DBManager.java
@@ -2,11 +2,13 @@ package dbmanager;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 class DBManager {
     //класс для управления подключениями к БД
     private Connection connection;
+    private PreparedStatement preparedStatement;
 
     DBManager() {
         try {
@@ -22,6 +24,7 @@ class DBManager {
     public void disconnect() {
         try {
             if (!connection.isClosed()) connection.close();
+            if (preparedStatement != null) preparedStatement.close();
         } catch (SQLException e) {
             e.printStackTrace();
         }
@@ -32,8 +35,15 @@ class DBManager {
     }
 
     //проверка на существование записей
-    public boolean checkExistence () {
-
+    public boolean checkExistence (String preparedQuery, String ... params) {
+        try {
+            preparedStatement = connection.prepareStatement(preparedQuery);
+            for (int i = 0; i < params.length; i++) {
+                preparedStatement.setString(i+1, params[i]);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
         return false;
     }
 
