dbeb8037bdcdc96128b583a701c7946f13e37fad
1PALADIN1 <sonikx94@yandex.ru>
2018-04-03 10:10:44 +0300

    Добавил команду AUTHERROR

--- common/src/Command.java
+++ common/src/Command.java
@@ -1,6 +1,6 @@
 public enum Command {
     //команды
-    REG("/reg"), AUTH("/auth"), AUTHOK("/authok"), LS("/ls"), UPLOAD("/upload"), DOWNLOAD("/upload"), DELETE("/del"), NONE("/none");
+    REG("/reg"), AUTH("/auth"), AUTHOK("/authok"), AUTHERROR("/autherror"), LS("/ls"), UPLOAD("/upload"), DOWNLOAD("/upload"), DELETE("/del"), NONE("/none");
 
     private String commandString;
 
@@ -14,9 +14,10 @@ public enum Command {
 
     public static Command getCommand(String commandCode) {
         switch (commandCode) {
-            case "/reg": return REG;
-            case "/auth": return AUTH;
-            case "/authok": return AUTHOK;
+            case "/reg": return REG; //запрос на регистрацию
+            case "/auth": return AUTH; //запрос на авторизацию
+            case "/authok": return AUTHOK; //авторизация успешна
+            case "/autherror": return AUTHERROR; //ошибки при авторизации
             case "/ls": return LS;
             default:
                 return NONE; //команда не распознана
