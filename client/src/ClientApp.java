8665e29a7e62ae7791e0ef478aeb3063cc7e8bab
1PALADIN1 <sonikx94@yandex.ru>
2018-04-03 10:03:19 +0300

    Почистил код

--- client/src/ClientApp.java
+++ client/src/ClientApp.java
@@ -4,8 +4,11 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import scenemanager.SceneManager;
 
 public class ClientApp extends Application {
+    private Stage primaryStage;
+    private SceneManager sceneManager;
 
     public static void main(String[] args) {
         launch(args);
@@ -13,6 +16,10 @@ public class ClientApp extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
+        this.primaryStage = primaryStage;
+        this.sceneManager = new SceneManager();
+        sceneManager.setPrimaryStage(primaryStage);
+
         Parent root = FXMLLoader.load(getClass().getResource("templates/login.fxml"));
         primaryStage.setTitle("DropStore GUI Client");
         primaryStage.setScene(new Scene(root, 800, 600));
